c947ab5aa05e4336d6fe3ee139130a0686cab006
Wellington Honrio <wellington.hm.w@gmail.com>
2021-09-26 19:17:32 -0300

    Ajuste de bugs visuais e inserção de conteudo

--- js/main.js
+++ js/main.js
@@ -53,8 +53,35 @@ function backToTop() {
   }
 }
 
+/* Menu ativo conforme a seção visivel na página */
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
 /* When Scroll */
 window.addEventListener('scroll', function () {
-  changeHeaderWhenScroll()
-  backToTop()
+  changeHeaderWhenScroll();
+  backToTop();
+  activateMenuAtCurrentSection();
 })
